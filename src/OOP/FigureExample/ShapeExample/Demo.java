ce9cb56c01cbcd28963796b744472ede5c8a3b8c
Maksimo <MaksimoRamaj@Gmail.com>
2024-04-13 13:40:01 +0200

    -

--- src/OOP/FigureExample/ShapeExample/Demo.java
+++ src/OOP/FigureExample/ShapeExample/Demo.java
@@ -2,12 +2,15 @@ package OOP.FigureExample.ShapeExample;
 
 public class Demo {
     public static void main(String[] args) {
-        Shape sh1 = new Shape();
-        Circle c1 = new Circle();
-        Square sq1 = new Square();
-
+        Shape sh1;
+        Circle c1 = new Circle(5);
+        Square sq1 = new Square(5);
         sh1 = c1;
-        sh1.draw();
+        System.out.println(sh1.area());
+        System.out.println(c1.area());
+        sh1 = sq1;
+       System.out.println( sh1.area() );
+        System.out.println(sq1.area());
 
     }
 }
--- src/OOP/FigureExample/ShapeExample/Shape.java
+++ src/OOP/FigureExample/ShapeExample/Shape.java
@@ -1,20 +1,55 @@
 package OOP.FigureExample.ShapeExample;
 
-class Shape {
+abstract class Shape {
     void draw() {
         System.out.println("Drawing a shape.");
     }
+    abstract double area();
+    abstract double perimter();
 }
-class Circle extends Shape {
-    @Override
+ class Circle extends Shape {
+    private int radius;
+
+     public Circle(int radius) {
+         this.radius = radius;
+     }
+
+     @Override
     void draw() {
         System.out.println("Drawing a circle.");
     }
-}
+
+     @Override
+     double area() {
+         System.out.println("Brenda rrethit!");
+        return Math.PI * Math.pow(radius,2);
+     }
+     @Override
+     double perimter() {
+         return 2*Math.PI * radius;
+     }
+ }
 class Square extends Shape {
+    int a;
+
+    public Square(int a) {
+        this.a = a;
+    }
+
     @Override
     void draw() {
         System.out.println("Drawing a square.");
+    }
+
+    @Override
+    double area() {
+        System.out.println("Brenda katrorit!");
+        return a * a;
+    }
+
+    @Override
+    double perimter() {
+        return 0;
     }
 }
 
